ef5c67a9b4f01dcee9f6f0eca858215c2cfd964d
Holden B <holden.bucher.92@gmail.com>
2019-01-29 14:24:43 -0600

    last MVP tests

--- baseball/src/components/Dashboard.spec.js
+++ baseball/src/components/Dashboard.spec.js
@@ -44,4 +44,120 @@ describe('<Dashboard />', () => {
 			expect(selected).toHaveTextContent(/0 balls/i);
 		});
 	});
+
+	describe('foul ball button', () => {
+		const { getByText, getByTestId } = render(<Dashboard />);
+		const selected = getByTestId('strikes');
+		const button = getByText(/foul ball/i);
+		it('should go from 0 to 1 strikes', () => {
+			fireEvent.click(button);
+			expect(selected).toHaveTextContent(/1 strikes/i);
+		});
+		it('should go from 1 to 2 strikes', () => {
+			fireEvent.click(button);
+			expect(selected).toHaveTextContent(/2 strikes/i);
+		});
+		it('should not go past 2 strikes', () => {
+			fireEvent.click(button);
+			fireEvent.click(button);
+			fireEvent.click(button);
+			fireEvent.click(button);
+			expect(selected).toHaveTextContent(/2 strikes/i);
+		});
+	});
+
+	describe('hit button', () => {
+		const { getByText, getByTestId } = render(<Dashboard />);
+		const strike = getByTestId('strikes');
+		const ball = getByTestId('balls');
+		const hitButton = getByText(/hit/i);
+		const strikeButton = getByText(/add strike/i);
+		const ballButton = getByText(/add ball/i);
+		it('should zero out 1 strike and 0 balls', () => {
+			fireEvent.click(strikeButton);
+			fireEvent.click(hitButton);
+			expect(strike).toHaveTextContent(/0 strikes/i);
+			expect(ball).toHaveTextContent(/0 balls/i);
+		});
+
+		it('should zero out 1 strike and 1 ball', () => {
+			fireEvent.click(strikeButton);
+			fireEvent.click(ballButton);
+			fireEvent.click(hitButton);
+			expect(strike).toHaveTextContent(/0 strikes/i);
+			expect(ball).toHaveTextContent(/0 balls/i);
+		});
+		it('should zero out 1 strike and 2 balls', () => {
+			fireEvent.click(ballButton);
+			fireEvent.click(strikeButton);
+			fireEvent.click(ballButton);
+			fireEvent.click(hitButton);
+			expect(strike).toHaveTextContent(/0 strikes/i);
+			expect(ball).toHaveTextContent(/0 balls/i);
+		});
+		it('should zero out 1 strike and 3 balls', () => {
+			fireEvent.click(ballButton);
+			fireEvent.click(ballButton);
+			fireEvent.click(strikeButton);
+			fireEvent.click(ballButton);
+			fireEvent.click(hitButton);
+			expect(strike).toHaveTextContent(/0 strikes/i);
+			expect(ball).toHaveTextContent(/0 balls/i);
+		});
+		it('should zero out 2 strikes and 0 balls', () => {
+			fireEvent.click(strikeButton);
+			fireEvent.click(strikeButton);
+			fireEvent.click(hitButton);
+			expect(strike).toHaveTextContent(/0 strikes/i);
+			expect(ball).toHaveTextContent(/0 balls/i);
+		});
+		it('should zero out 2 strikes and 1 balls', () => {
+			fireEvent.click(strikeButton);
+			fireEvent.click(strikeButton);
+			fireEvent.click(ballButton);
+			fireEvent.click(hitButton);
+			expect(strike).toHaveTextContent(/0 strikes/i);
+			expect(ball).toHaveTextContent(/0 balls/i);
+		});
+		it('should zero out 2 strikes and 2 balls', () => {
+			fireEvent.click(strikeButton);
+			fireEvent.click(strikeButton);
+			fireEvent.click(ballButton);
+			fireEvent.click(ballButton);
+			fireEvent.click(hitButton);
+			expect(strike).toHaveTextContent(/0 strikes/i);
+			expect(ball).toHaveTextContent(/0 balls/i);
+		});
+		it('should zero out 2 strikes and 3 balls', () => {
+			fireEvent.click(strikeButton);
+			fireEvent.click(strikeButton);
+			fireEvent.click(ballButton);
+			fireEvent.click(ballButton);
+			fireEvent.click(ballButton);
+			fireEvent.click(hitButton);
+			expect(strike).toHaveTextContent(/0 strikes/i);
+			expect(ball).toHaveTextContent(/0 balls/i);
+		});
+		it('should zero out 0 strikes and 1 ball', () => {
+			fireEvent.click(ballButton);
+			fireEvent.click(hitButton);
+			expect(strike).toHaveTextContent(/0 strikes/i);
+			expect(ball).toHaveTextContent(/0 balls/i);
+		});
+		it('should zero out 0 strikes and 2 balls', () => {
+			fireEvent.click(ballButton);
+			fireEvent.click(ballButton);
+			fireEvent.click(hitButton);
+			expect(strike).toHaveTextContent(/0 strikes/i);
+			expect(ball).toHaveTextContent(/0 balls/i);
+		});
+		it('should zero out 0 strikes and 3 balls', () => {
+			fireEvent.click(ballButton);
+			fireEvent.click(ballButton);
+			fireEvent.click(ballButton);
+			fireEvent.click(hitButton);
+			expect(strike).toHaveTextContent(/0 strikes/i);
+			expect(ball).toHaveTextContent(/0 balls/i);
+		});
+	});
 });
